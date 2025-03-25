48cb0dbe36b4ffe2fbc66da1eec2004a439a082e
Irene Alfaro <irealfaro91@gmail.com>
2025-03-25 19:08:07 +0100

    paragraph commit

---
+++ src/dom.js
@@ -0,0 +1,6 @@
+import function createParagraph(){
+ 
+     const paragraph = document.createElement('p');
+     paragraph.innerText = 'Ingresa tu número de teléfono';
+    return paragraph;        
+}
