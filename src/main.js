4df5a6b14266f1031bbb3c92f26526e332f62016
elena <elenaobregonl@gmail.com>
2025-03-25 20:17:48 +0100

    showMessages

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-import './scss/style.scss'
+import "./scss/style.scss";
+
+function showMessages(phoneNumber, valid) {
+  let message;
+
+  message = valid
+    ? phoneNumber.slice(-3).padStart(phoneNumber.length, "*")
+    : "Invalid phone number";
+
+  const elem = document.querySelector("p");
+  elem.classList = valid ? "valid" : "error";
+  elem.textContent = message;
+}
+
+
